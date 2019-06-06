51c32e52cb54c30d720c03232f4799e48698cfae
Son NK <nguyenkims@hotmail.com>
2019-06-06 22:09:31 +0200

    no need to interfere with root logger

--- app/log.py
+++ app/log.py
@@ -6,13 +6,10 @@ _log_format = "%(asctime)s - %(name)s - %(levelname)s - %(process)d - %(module)s
 _log_formatter = logging.Formatter(_log_format)
 
 
-def _get_console_handler(level=None):
+def _get_console_handler():
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(_log_formatter)
     console_handler.formatter.converter = time.gmtime
-
-    if level:
-        console_handler.setLevel(level)
 
     return console_handler
 
@@ -25,21 +22,12 @@ def get_logger(name):
     # leave the handlers level at NOTSET so the level checking is only handled by the logger
     logger.addHandler(_get_console_handler())
 
-    # no propagation to avoid unexpected behaviour
+    # no propagation to avoid propagating to root logger
     logger.propagate = False
 
     return logger
 
 
 print(f">>> init logging <<<")
-
-# ### config root logger ###
-# do not use the default (buggy) logger
-logging.root.handlers.clear()
-
-# add handlers with the default level = "warn"
-# need to add level at handler level as there's no level check in root logger
-# all the libs logs having level >= WARN will be handled by these 2 handlers
-logging.root.addHandler(_get_console_handler(logging.WARN))
 
 LOG = get_logger("yourkey")
