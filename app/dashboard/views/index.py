e9e0451ff605d144b9e49b98cb2a9d1f9322f47d
Son NK <son@workwell.io>
2019-06-07 12:32:27 +0200

    display list of clients that have user has authorised

--- app/dashboard/views/index.py
+++ app/dashboard/views/index.py
@@ -1,10 +1,19 @@
 from flask import render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from sqlalchemy.orm import joinedload
 
 from app.dashboard.base import dashboard_bp
+from app.models import ClientUser
 
 
 @dashboard_bp.route("/")
 @login_required
 def index():
-    return render_template("dashboard/index.html")
+    client_users = (
+        ClientUser.filter_by(user_id=current_user.id)
+        .options(joinedload(ClientUser.client))
+        .options(joinedload(ClientUser.gen_email))
+        .all()
+    )
+
+    return render_template("dashboard/index.html", client_users=client_users)
