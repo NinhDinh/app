2bb5e315ca77a668c9f3739f0ee6b1b8848f6d6f
Son NK <son@workwell.io>
2019-06-08 20:46:57 +0200

    EMAIL_DOMAIN can come from env var

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
+import os
+
 SCOPE_NAME = "name"
 SCOPE_EMAIL = "email"
 
-EMAIL_DOMAIN = "yourkey.io"
+EMAIL_DOMAIN = os.environ.get("EMAIL_DOMAIN") or "yourkey.io"
+
+print("email domain is", EMAIL_DOMAIN)
