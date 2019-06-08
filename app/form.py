1e9140ebe55c114a821baf203333b31aeb2ed2e0
Son NK <son@workwell.io>
2019-06-08 21:03:34 +0200

    fix signup error: use session as default csrf_context

--- app/form.py
+++ app/form.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from flask import session
 from wtforms import Form
 from wtforms.csrf.session import SessionCSRF
 
@@ -10,3 +11,7 @@ class BaseForm(Form):
         csrf_class = SessionCSRF
         csrf_secret = b"TODO"
         csrf_time_limit = timedelta(minutes=20)
+
+        @property
+        def csrf_context(self):
+            return session
