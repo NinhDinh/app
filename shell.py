ec27d22545605948db43f2503aa96796503409fc
Son NK <son@workwell.io>
2019-06-09 23:08:44 +0200

    add drop_db into shell

--- shell.py
+++ shell.py
@@ -1,5 +1,5 @@
 from IPython import embed
-from sqlalchemy_utils import create_database, database_exists
+from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from app.config import DB_URI
 from app.extensions import db
@@ -17,6 +17,10 @@ def create_db():
     with app.app_context():
         # Create all tables
         db.create_all()
+
+
+def drop_db():
+    drop_database(DB_URI)
 
 
 embed()
