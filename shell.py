3d5be66f4d64d6214e9fdc059862f09c1656c2de
Son NK <son@workwell.io>
2019-06-09 13:54:55 +0200

    create shell

---
+++ shell.py
@@ -0,0 +1,22 @@
+from IPython import embed
+from sqlalchemy_utils import create_database, database_exists
+
+from app.config import DB_URI
+from app.extensions import db
+from app.log import LOG
+from server import create_app
+
+
+def create_db():
+    if not database_exists(DB_URI):
+        LOG.debug("db not exist, create database")
+        create_database(DB_URI)
+
+    app = create_app()
+
+    with app.app_context():
+        # Create all tables
+        db.create_all()
+
+
+embed()
