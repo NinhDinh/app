5dcfca3419ac1d0b9b911b0d0b781ce0dbaf345b
Son NK <son@workwell.io>
2019-06-10 23:45:10 +0200

    user can generate new email

--- app/dashboard/views/index.py
+++ app/dashboard/views/index.py
@@ -1,14 +1,27 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from sqlalchemy.orm import joinedload
 
 from app.dashboard.base import dashboard_bp
-from app.models import ClientUser
+from app.extensions import db
+from app.log import LOG
+from app.models import GenEmail, ClientUser
+from app.oauth.views.authorize import generate_email
 
 
-@dashboard_bp.route("/")
+@dashboard_bp.route("/", methods=["GET", "POST"])
 @login_required
 def index():
+    # User generates a new email
+    if request.method == "POST":
+        random_email = generate_email()
+        GenEmail.create(user_id=current_user.id, email=random_email)
+        db.session.commit()
+
+        LOG.d("generate new email %s for user %s", random_email, current_user)
+
+        return redirect(url_for("dashboard.index"))
+
     client_users = (
         ClientUser.filter_by(user_id=current_user.id)
         .options(joinedload(ClientUser.client))
