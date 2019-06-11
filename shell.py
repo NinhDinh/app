d6907dc73a0c4526f84ef9f09f5db3071f222dff
Son NK <son@workwell.io>
2019-06-11 12:27:50 +0200

    refactor shell: remove prepare_db()

--- shell.py
+++ shell.py
@@ -2,9 +2,8 @@ from IPython import embed
 from sqlalchemy_utils import create_database, database_exists, drop_database
 
 from app.config import DB_URI, SCOPE_NAME, SCOPE_EMAIL
-from app.extensions import db
 from app.log import LOG
-from app.models import Scope
+from app.models import *
 from server import create_app
 
 
@@ -19,17 +18,6 @@ def create_db():
         # Create all tables
         db.create_all()
 
-    prepare_db()
-
-
-def drop_db():
-    drop_database(DB_URI)
-
-
-def prepare_db():
-    app = create_app()
-
-    with app.app_context():
         scope_name = Scope.create(name=SCOPE_NAME)
         db.session.add(scope_name)
         scope_email = Scope.create(name=SCOPE_EMAIL)
@@ -37,4 +25,11 @@ def prepare_db():
         db.session.commit()
 
 
-embed()
+def drop_db():
+    drop_database(DB_URI)
+
+
+app = create_app()
+
+with app.app_context():
+    embed()
