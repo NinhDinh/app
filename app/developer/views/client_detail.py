ff5fa1b107736070e43217004239c33a378743ff
Son NK <nguyenkims@hotmail.com>
2019-06-12 22:04:25 +0200

    handle client icon update

--- app/developer/views/client_detail.py
+++ app/developer/views/client_detail.py
@@ -1,21 +1,29 @@
+from io import BytesIO
+
 from flask import request, render_template, redirect, url_for, flash
 from flask_login import current_user, login_required
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField
 from wtforms import StringField, validators
 
+from app import s3
 from app.developer.base import developer_bp
 from app.extensions import db
-from app.models import Client, RedirectUri
+from app.log import LOG
+from app.models import Client, RedirectUri, File
+from app.utils import random_string
 
 
 class EditClientForm(FlaskForm):
     name = StringField("Name", validators=[validators.DataRequired()])
+    icon = FileField("Icon")
 
 
 @developer_bp.route("/clients/<client_id>", methods=["GET", "POST"])
 @login_required
 def client_detail(client_id):
+    form = EditClientForm()
+
     client = Client.get(client_id)
     if not client:
         flash("no such client", "warning")
@@ -25,13 +33,23 @@ def client_detail(client_id):
         flash("you cannot see this client", "warning")
         return redirect(url_for("developer.index"))
 
-    if request.method == "GET":
-        form = EditClientForm(name=client.name)
-    else:
-        form = EditClientForm(request.form)
-
+    if request.method == "POST":
         if form.validate():
             client.name = form.name.data
+
+            if form.icon.data:
+                # todo: remove current icon if any
+                # todo: handle remove icon
+                file_path = random_string(30)
+                file = File.create(path=file_path)
+
+                s3.upload_from_bytesio(file_path, BytesIO(form.icon.data.read()))
+
+                db.session.commit()
+                LOG.d("upload file %s to s3", file)
+
+                client.icon_id = file.id
+                db.session.commit()
 
             uris = request.form.getlist("uri")
 
