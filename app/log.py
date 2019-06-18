f106dd497b59e842d8c17f3a3f4851f08110e59f
Son NK <son@workwell.io>
2019-06-18 16:59:29 +0200

    add cloudwatch logger if cloudwatch is enabled

--- app/log.py
+++ app/log.py
@@ -2,6 +2,18 @@ import logging
 import sys
 import time
 
+import boto3
+import watchtower
+
+from app.config import (
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION,
+    CLOUDWATCH_LOG_GROUP,
+    ENABLE_CLOUDWATCH,
+    CLOUDWATCH_LOG_STREAM,
+)
+
 _log_format = "%(asctime)s - %(name)s - %(levelname)s - %(process)d - %(module)s:%(lineno)d - %(funcName)s - %(message)s"
 _log_formatter = logging.Formatter(_log_format)
 
@@ -14,13 +26,41 @@ def _get_console_handler():
     return console_handler
 
 
-def get_logger(name):
+def _get_watchtower_handler():
+    session = boto3.Session(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_REGION,
+    )
+
+    handler = watchtower.CloudWatchLogHandler(
+        log_group=CLOUDWATCH_LOG_GROUP,
+        stream_name=CLOUDWATCH_LOG_STREAM,
+        send_interval=5,  # every 5 sec
+        boto3_session=session,
+    )
+
+    handler.setFormatter(_log_formatter)
+
+    return handler
+
+
+def _get_logger(name):
     logger = logging.getLogger(name)
 
     logger.setLevel(logging.DEBUG)
 
     # leave the handlers level at NOTSET so the level checking is only handled by the logger
     logger.addHandler(_get_console_handler())
+
+    if ENABLE_CLOUDWATCH:
+        print(
+            "enable cloudwatch, log group",
+            CLOUDWATCH_LOG_GROUP,
+            "; log stream:",
+            CLOUDWATCH_LOG_STREAM,
+        )
+        logger.addHandler(_get_watchtower_handler())
 
     # no propagation to avoid propagating to root logger
     logger.propagate = False
@@ -34,4 +74,4 @@ print(f">>> init logging <<<")
 logging.Logger.d = logging.Logger.debug
 logging.Logger.i = logging.Logger.info
 
-LOG = get_logger("sl")
+LOG = _get_logger("sl")
