1a1fc92e53a597213c623d972c04bab5246bdae1
Son NK <nguyenkims@hotmail.com>
2019-06-24 09:28:27 +0200

    use sendgrid to notify admin

--- app/email_utils.py
+++ app/email_utils.py
@@ -1,13 +1,10 @@
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
-from app.config import SUPPORT_EMAIL, SENDGRID_API_KEY, EMAIL_DOMAIN
+from app.config import SUPPORT_EMAIL, SENDGRID_API_KEY
 from app.log import LOG
 
 
@@ -24,24 +21,11 @@ def send(to_email, subject, html_content):
 
 
 def notify_admin(subject, html_content):
-    """only works when there's a postfix server running locally"""
-    msg = MIMEMultipart("alternative")
-
-    fromaddr = f"noreply@{EMAIL_DOMAIN}"
-    toaddr = SUPPORT_EMAIL
-
-    msg["From"] = fromaddr
-    msg["To"] = toaddr
-    msg["Subject"] = subject
-
-    msg.attach(
-        MIMEText(
-            f"""<html><body>
+    send(
+        SUPPORT_EMAIL,
+        subject,
+        f"""
+        <html><body>
     {html_content}
     </body></html>""",
-            "html",
-        )
     )
-
-    with smtplib.SMTP(host="localhost") as server:
-        server.sendmail(fromaddr, toaddr, msg.as_string())
